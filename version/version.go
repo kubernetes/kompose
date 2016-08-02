bc7705157ade6dbf2e8d5c7b142257e0b38ec223
Tuna <ng.tuna@gmail.com>
2016-08-02 14:25:40 +0700

    correct kompose version to 0.0.1-beta

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ package version
 
 var (
 // VERSION should be updated by hand at each release
-	VERSION = "0.0.1-alpha"
+	VERSION = "0.0.1-beta"
 
 // GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
