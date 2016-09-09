822768446621d71ffd02eafd138db0d8ab4a7d0e
Tuna <ng.tuna@gmail.com>
2016-09-09 16:46:31 +0700

    bump kompose version to v0.1.0

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "0.0.1-beta"
+	VERSION = "0.1.0"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
