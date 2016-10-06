3306a4ff0c976efc7334ccabbe009837f3ed3a6a
Tuna <ng.tuna@gmail.com>
2016-10-06 21:54:17 +0200

    0.1.1 is coming

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ package version
 
 var (
 	// VERSION should be updated by hand at each release
-	VERSION = "dev"
+	VERSION = "0.1.1"
 
 	// GITCOMMIT will be overwritten automatically by the build system
 	GITCOMMIT = "HEAD"
