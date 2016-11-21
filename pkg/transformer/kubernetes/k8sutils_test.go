74ccf7c21f4aafd4211d7baec944b0a10ed8cc95
Tomas Kral <tkral@redhat.com>
2016-11-21 13:13:03 +0100

    Add tests for user directive

--- pkg/transformer/kubernetes/k8sutils_test.go
+++ pkg/transformer/kubernetes/k8sutils_test.go
@@ -17,10 +17,12 @@ limitations under the License.
 package kubernetes
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/kubernetes-incubator/kompose/pkg/kobject"
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
 )
 
 /*
@@ -49,7 +51,7 @@ func TestCreateService(t *testing.T) {
 		Expose:        []string{"expose"},   // not supported
 		Privileged:    true,
 		Restart:       "always",
-		User:          "user", // not supported
+		User:          "user",
 	}
 
 	// An example object generated via k8s runtime.Objects()
@@ -64,4 +66,52 @@ func TestCreateService(t *testing.T) {
 	if svc.Spec.Ports[0].Port != 123 {
 		t.Errorf("Expected port 123 upon conversion, actual %d", svc.Spec.Ports[0].Port)
 	}
+}
+
+/*
+	Test the creation of a service with a specified user.
+	The expected result is that Kompose will set user in PodSpec
+*/
+func TestCreateServiceWithServiceUser(t *testing.T) {
+
+	// An example service
+	service := kobject.ServiceConfig{
+		ContainerName: "name",
+		Image:         "image",
+		Environment:   []kobject.EnvVar{kobject.EnvVar{Name: "env", Value: "value"}},
+		Port:          []kobject.Ports{kobject.Ports{HostPort: 123, ContainerPort: 456, Protocol: api.ProtocolTCP}},
+		Command:       []string{"cmd"},
+		WorkingDir:    "dir",
+		Args:          []string{"arg1", "arg2"},
+		Volumes:       []string{"/tmp/volume"},
+		Network:       []string{"network1", "network2"}, // not supported
+		Labels:        nil,
+		Annotations:   map[string]string{"kompose.service.type": "nodeport"},
+		CPUSet:        "cpu_set",            // not supported
+		CPUShares:     1,                    // not supported
+		CPUQuota:      1,                    // not supported
+		CapAdd:        []string{"cap_add"},  // not supported
+		CapDrop:       []string{"cap_drop"}, // not supported
+		Expose:        []string{"expose"},   // not supported
+		Privileged:    true,
+		Restart:       "always",
+		User:          "1234",
+	}
+
+	komposeObject := kobject.KomposeObject{
+		ServiceConfigs: map[string]kobject.ServiceConfig{"app": service},
+	}
+	k := Kubernetes{}
+
+	objects := k.Transform(komposeObject, kobject.ConvertOptions{CreateD: true, Replicas: 1})
+
+	for _, obj := range objects {
+		if deploy, ok := obj.(*extensions.Deployment); ok {
+			uid := *deploy.Spec.Template.Spec.Containers[0].SecurityContext.RunAsUser
+			if strconv.FormatInt(uid, 10) != service.User {
+				t.Errorf("User in ServiceConfig is not matching user in PodSpec")
+			}
+		}
+	}
+
 }
