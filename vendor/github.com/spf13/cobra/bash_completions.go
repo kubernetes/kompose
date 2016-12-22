1fdc1f648e968867bfe5f052146e5ed1fc5ce680
Charlie Drage <charlie@charliedrage.com>
2016-12-22 09:18:22 -0500

    Update vendoring

--- vendor/github.com/spf13/cobra/bash_completions.go
+++ vendor/github.com/spf13/cobra/bash_completions.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	BashCompFilenameExt     = "cobra_annotation_bash_completion_filename_extentions"
+	BashCompFilenameExt     = "cobra_annotation_bash_completion_filename_extensions"
 	BashCompCustom          = "cobra_annotation_bash_completion_custom"
 	BashCompOneRequiredFlag = "cobra_annotation_bash_completion_one_required_flag"
 	BashCompSubdirsInDir    = "cobra_annotation_bash_completion_subdirs_in_dir"
@@ -401,10 +401,8 @@ func writeLocalNonPersistentFlag(flag *pflag.Flag, w io.Writer) error {
 		format += "="
 	}
 	format += "\")\n"
-	if _, err := fmt.Fprintf(w, format, name); err != nil {
-		return err
-	}
-	return nil
+	_, err := fmt.Fprintf(w, format, name)
+	return err
 }
 
 func writeFlags(cmd *Command, w io.Writer) error {
