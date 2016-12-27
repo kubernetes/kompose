9a243bfe042cfe9bdffed367440b1b86dc93ba1b
Ratnadeep Debnath <rtnpro@gmail.com>
2016-12-27 20:07:03 +0530

    Fixed typos in openshift buildconfig
    - spelling mistake
    - pass compose file dir instead of compose file to initBuildConfig call
    - Use as default value for cli --build-branch option
    - Pass current build branch to buildconfig related functions instead of opt.BuildBranch
    - Fix printing buildconfig source branch in logs.

--- cmd/convert.go
+++ cmd/convert.go
@@ -88,7 +88,7 @@ func init() {
 	convertCmd.Flags().MarkHidden("deployment-config")
 	convertCmd.Flags().StringVar(&ConvertBuildRepo, "build-repo", "", "Specify source repository for buildconfig (default remote origin)")
 	convertCmd.Flags().MarkHidden("build-repo")
-	convertCmd.Flags().StringVar(&ConvertBuildBranch, "build-branch", "master", "Specify repository branch to use for buildconfig (default master)")
+	convertCmd.Flags().StringVar(&ConvertBuildBranch, "build-branch", "", "Specify repository branch to use for buildconfig (default master)")
 	convertCmd.Flags().MarkHidden("build-branch")
 
 	// Standard between the two
