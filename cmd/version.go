c27bbc223c2bf0d668d53aee84a5d37de276481b
Charlie Drage <charlie@charliedrage.com>
2017-10-10 15:24:44 -0400

    1.3.0 Release

--- cmd/version.go
+++ cmd/version.go
@@ -24,7 +24,7 @@ import (
 
 var (
 	// VERSION  is version number that wil be displayed when running ./kompose version
-	VERSION = "1.2.0"
+	VERSION = "1.3.0"
 	// GITCOMMIT is hash of the commit that wil be displayed when running ./kompose version
 	// this will be overwritten when running  build like this: go build -ldflags="-X github.com/kubernetes/kompose/cmd.GITCOMMIT=$(GITCOMMIT)"
 	// HEAD is default indicating that this was not set during build
