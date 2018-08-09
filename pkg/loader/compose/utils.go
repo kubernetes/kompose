208387fa60661838900aeff66327d98d6f409267
Hang Yan <hangyan@alauda.io>
2018-08-09 22:46:41 +0800

    Support read data from stdin

--- pkg/loader/compose/utils.go
+++ pkg/loader/compose/utils.go
@@ -17,6 +17,7 @@ limitations under the License.
 package compose
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -114,4 +115,19 @@ func handleServiceType(ServiceType string) (string, error) {
 
 func normalizeServiceNames(svcName string) string {
 	return strings.Replace(svcName, "_", "-", -1)
+}
+
+// ReadFile read data from file or stdin
+func ReadFile(fileName string) ([]byte, error) {
+	if fileName == "-" {
+		if StdinData == nil {
+			data, err := ioutil.ReadAll(os.Stdin)
+			StdinData = data
+			return data, err
+		} else {
+			return StdinData, nil
+		}
+	}
+	return ioutil.ReadFile(fileName)
+
 }
