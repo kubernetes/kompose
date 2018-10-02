4c0fa2f109c2e64ea9d8c11747a3435ee73dcca2
Risto Treksler <treksler@ucalgary.ca>
2018-10-01 19:52:55 -0600

    add annotations to ingress

--- pkg/transformer/kubernetes/kubernetes.go
+++ pkg/transformer/kubernetes/kubernetes.go
@@ -332,6 +332,7 @@ func (k *Kubernetes) initIngress(name string, service kobject.ServiceConfig, por
 		ObjectMeta: api.ObjectMeta{
 			Name:   name,
 			Labels: transformer.ConfigLabels(name),
+			Annotations:  transformer.ConfigAnnotations(service),
 		},
 		Spec: extensions.IngressSpec{
 			Rules: []extensions.IngressRule{
