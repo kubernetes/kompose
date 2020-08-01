47e6015f5027328c4fd5dedb78261fa90e35f0d1
Hang Yan <hangyan@alauda.io>
2020-08-01 16:01:34 +0800

    Remove up and down cmd (#1297)
    * Remove up and down cmd

--- vendor/github.com/openshift/origin/pkg/auth/authenticator/request/x509request/doc.go
+++
@@ -1,3 +0,0 @@
-// Package x509request provides a request authenticator that validates and
-// extracts user information from client certificates
-package x509request
--- vendor/github.com/openshift/origin/pkg/cmd/util/doc.go
+++
@@ -1,2 +0,0 @@
-// Package util provides utility functions for the cmd packages.
-package util
