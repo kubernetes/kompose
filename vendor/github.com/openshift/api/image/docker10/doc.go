63509cd499df28a88ce185e6db9646d4c74fa26b
Hang Yan <hangyan@alauda.io>
2020-08-07 17:45:29 +0800

    Remove vendor

--- vendor/github.com/openshift/api/image/docker10/doc.go
+++
@@ -1,4 +0,0 @@
-// +k8s:deepcopy-gen=package,register
-
-// Package docker10 is the docker10 version of the API.
-package docker10
--- vendor/github.com/openshift/api/image/dockerpre012/doc.go
+++
@@ -1,4 +0,0 @@
-// +k8s:deepcopy-gen=package,register
-
-// Package dockerpre012 is the dockerpre012 version of the API.
-package dockerpre012
