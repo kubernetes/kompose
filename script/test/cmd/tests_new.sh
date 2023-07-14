61b4e6b79704f6a3edada19c3dc7613ed5c9bb23
AhmedGrati <ahmedgrati1999@gmail.com>
2023-07-14 16:24:43 +0100

    test: add functional tests for read only fs support
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- script/test/cmd/tests_new.sh
+++ script/test/cmd/tests_new.sh
@@ -277,3 +277,11 @@ convert::expect_success "$os_cmd" "$os_output"
 k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/custom-build-push/docker-compose.yaml convert --build-command 'docker build -t ahmedgrati/kompose-test ./script/test/fixtures/custom-build-push' --push-command 'docker push ahmedgrati/kompose-test' --stdout --with-kompose-annotation=false"
 k8s_output="$KOMPOSE_ROOT/script/test/fixtures/custom-build-push/output-k8s.yaml"
 convert::expect_success "$os_cmd" "$os_output"
+
+# Test support for read only root fs
+k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/read-only/docker-compose.yaml convert --stdout --with-kompose-annotation=false"
+k8s_output="$KOMPOSE_ROOT/script/test/fixtures/read-only/output-k8s.yaml"
+os_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/read-only/docker-compose.yaml convert --stdout --with-kompose-annotation=false --provider openshift"
+os_output="$KOMPOSE_ROOT/script/test/fixtures/read-only/output-os.yaml"
+convert::expect_success "$k8s_cmd" "$k8s_output"
+convert::expect_success "$os_cmd" "$os_output"
