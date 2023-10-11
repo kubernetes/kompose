115f3862fcbf2916d13f263e565d1151040f9a9e
AhmedGrati <ahmedgrati1999@gmail.com>
2023-10-11 20:09:07 +0100

    fix: update part of e2e tests
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- script/test/cmd/tests_new.sh
+++ script/test/cmd/tests_new.sh
@@ -250,7 +250,7 @@ convert::expect_success_and_warning "$k8s_cmd" "$k8s_output"
 convert::expect_success_and_warning "$os_cmd" "$os_output"
 
 # Test support for compose.yaml file
-k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/compose-file-support/docker-compose.yaml convert --stdout --with-kompose-annotation=false"
+k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/compose-file-support/compose.yaml convert --stdout --with-kompose-annotation=false"
 k8s_output="$KOMPOSE_ROOT/script/test/fixtures/compose-file-support/output-k8s.yaml"
 convert::expect_success "$k8s_cmd" "$k8s_output"
 
@@ -271,11 +271,6 @@ convert::expect_success "$os_cmd" "$os_output"
 # Test support for network policies generation
 k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/network-policies/docker-compose.yaml convert --generate-network-policies --stdout --with-kompose-annotation=false"
 k8s_output="$KOMPOSE_ROOT/script/test/fixtures/network-policies/output-k8s.yaml"
-convert::expect_success "$k8s_cmd" "$k8s_output"
-
-# Test support for custom build and push images
-k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/custom-build-push/docker-compose.yaml convert --build-command 'docker build -t ahmedgrati/kompose-test ./script/test/fixtures/custom-build-push' --push-command 'docker push ahmedgrati/kompose-test' --stdout --with-kompose-annotation=false"
-k8s_output="$KOMPOSE_ROOT/script/test/fixtures/custom-build-push/output-k8s.yaml"
 convert::expect_success "$k8s_cmd" "$k8s_output"
 
 # Test support for namespace generation
