1f7441845e642aed23ba6d5d282a54c8440e203e
Charlie Drage <charlie@charliedrage.com>
2024-06-02 14:46:39 -0400

    bug: fix annotation bug (#1887)
    #### What type of PR is this?
    
    <!--
    Add one of the following kinds:
    /kind bug
    /kind cleanup
    /kind documentation
    /kind feature
    -->
    
    #### What this PR does / why we need it:
    
    When supplying a label in the compose file, it should stay in the output
    too.
    
    #### Which issue(s) this PR fixes:
    <!--
    *Automatically closes linked issue when PR is merged.
    Usage: `Fixes #<issue number>`, or `Fixes (paste link of issue)`.
    -->
    
    Fixes https://github.com/kubernetes/kompose/issues/1885
    
    #### Special notes for your reviewer:
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- script/test/cmd/tests.sh
+++ script/test/cmd/tests.sh
@@ -386,3 +386,8 @@ os_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/envvars-with-status/compos
 os_output="$KOMPOSE_ROOT/script/test/fixtures/envvars-with-status/output-os.yaml"
 convert::expect_success "$k8s_cmd" "$k8s_output" || exit 1
 convert::expect_success "$os_cmd" "$os_output" || exit 1
+
+# Test label in compose.yaml appears in the output annotation
+k8s_cmd="kompose -f $KOMPOSE_ROOT/script/test/fixtures/label/compose.yaml convert --stdout --with-kompose-annotation=false"
+k8s_output="$KOMPOSE_ROOT/script/test/fixtures/label/output-k8s.yaml"
+convert::expect_success "$k8s_cmd" "$k8s_output" || exit 1
